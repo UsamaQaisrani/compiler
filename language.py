ca54ab2d4ef88527dd9fcd0ffff2d01f336687a9
Muhammad <usamaqaisranifps@gmail.com>
2025-09-19 20:17:41 +0100

    separated token type class and tokenized the source

--- language.py
+++ language.py
@@ -1,12 +1,7 @@
 from lexer import *
 
 def main():
-    source = "+- */"
+    source = "x = 42"
     lexer = Lexer(source)
-
-    token = lexer.getToken()
-    while token.kind != TokenType.EOF:
-        print(token.kind)
-        token = lexer.getToken()
 
 main()
--- lexer.py
+++ lexer.py
@@ -1,89 +1,24 @@
-import enum, sys
+from lexer_token import *
 
 class Lexer:
     def __init__(self, source):
         self.source = source
-        self.currChar = ""
-        self.currPos = -1
-        self.nextChar()
+        self.tokens = []
+        self.getTokens()
 
-    def nextChar(self):
-        self.currPos += 1
-        if self.currPos >= len(self.source):
-            self.currChar = "\0" #EOF
-        else:
-            self.currChar = self.source[self.currPos]
+    def getTokens(self):
+        for match in token_regex.finditer(self.source):
+            token_text = match.group(0)
+            token_type_name = match.lastgroup
+            assert token_type_name is not None, 'Matched token has no group name!'
+            token_type = TokenType[token_type_name] 
+            token = Token(token_text, token_type)
+            self.tokens.append(token)
+            print(token.kind, token.text)
 
-    def peek(self):
-        if self.currPos + 1 >= len(self.source):
-            return "\0"
-        else:
-            return self.source[self.currPos+1]
-    
-    def abort(self, message):
-        sys.exit(f"Lexing error. {message}")
 
-    def skipWhiteSpace(self):
-        while self.currChar == " " or self.currChar == "\t" or self.currChar == "\r":
-            self.nextChar()
+            
 
-    def skipComment(self):
-        pass
 
-    def getToken(self):
-        self.skipWhiteSpace()
-        token = None
-        if self.currChar == "+":
-            token = Token(self.currChar, TokenType.PLUS)
-        elif self.currChar == "-":
-            token = Token(self.currChar, TokenType.MINUS)
-        elif self.currChar == "*":
-            token = Token(self.currChar, TokenType.ASTERISK)
-        elif self.currChar == "/":
-            token = Token(self.currChar, TokenType.SLASH)
-        elif self.currChar == "\n":
-            token = Token(self.currChar, TokenType.NEWLINE)
-        elif self.currChar == "\0":
-            token = Token(self.currChar, TokenType.EOF)
-        else:
-            self.abort(f"Unkown token: {self.currChar}")
-
-        self.nextChar()
-        return token
         
-class Token:
-    def __init__(self, tokenText, tokenKind) -> None:
-        self.text = tokenText
-        self.kind = tokenKind
-
-class TokenType(enum.Enum):
-    EOF = -1
-    NEWLINE = 0
-    NUMBER = 1
-    IDENT = 2
-    STRING = 3
-
-    LABEL = 101
-    GOTO = 102
-    PRINT = 103
-    INPUT = 104
-    LET = 105
-    IF = 106
-    THEN = 107
-    ENDIF = 108
-    WHILE = 109
-    REPEAT = 110
-    ENDWHILE = 111
-
-    EQ = 201
-    PLUS = 202
-    MINUS = 203
-    ASTERISK = 204
-    SLASH = 205
-    EQEQ = 206
-    NOTEQ = 207
-    LT = 208
-    LTEQ = 209
-    GT = 210
-    GTEQ = 211
 
